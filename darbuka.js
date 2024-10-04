ee16190a1acf854c9e6ca1cef6c87df177841c62
omarna12 <omarnajjout@gmail.com>
2024-10-04 12:10:40 +0200

    Descripción de los cambios

---
+++ darbuka.js
@@ -0,0 +1,13 @@
+
+
+
+function w(){
+    document.getElementsByName('img'),innerHTML = "NO";
+}
+
+
+document.addEventListener('keydown',function (event){
+if(event.key == 'w' || event.key=="W")  {
+    w()
+}
+});
